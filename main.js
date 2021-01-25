5bd128006cdd30275365662c3fc9f623a83b1940
Tim <tim.silvester45@outlook.com>
2021-01-25 13:31:37 +1100

    Prepared scripting to interact with html

--- main.js
+++ main.js
@@ -4,20 +4,21 @@ String.prototype.toProperCase = function () {
 
 function playRound(p1Choice, p2Choice, score, round){
     const beats = {"rock": "scissors", "scissors": "paper", "paper": "rock"}
+    let str
     [p1, p2] = Object.keys(score)
     if (p1Choice == p2Choice){
-        console.log(`${p1} and ${p2} tie round ${round}! Both selected ${p1Choice.toProperCase()}`)
+        str = `${p1} and ${p2} tie round ${round}! Both selected ${p1Choice.toProperCase()}`
     } else {
         if (beats[p1Choice] == p2Choice){
             score[p1] += 1
-            console.log(`${p1} won round ${round} against ${p2}! ${p1Choice.toProperCase()} beats ${p2Choice.toProperCase()}`)
+            str = `${p1} won round ${round} against ${p2}! ${p1Choice.toProperCase()} beats ${p2Choice.toProperCase()}`
         } else {
             score[p2] += 1
-            console.log(`${p2} won round ${round} against ${p1}! ${p2Choice.toProperCase()} beats ${p1Choice.toProperCase()}`)
+            str = `${p2} won round ${round} against ${p1}! ${p2Choice.toProperCase()} beats ${p1Choice.toProperCase()}`
         }
         
     }
-    return score
+    return (score, str)
 }
 
 function playerMove(){
@@ -35,8 +36,10 @@ function computerMove(){
 function game(players, rounds){
     [p1, p2] = Object.keys(players)
     let score = {[p1]: 0, [p2]: 0}
+    let str
     for (let i = 1; i <= rounds; i++) {
-        score = playRound(players[p1](), players[p2](), score, i)            
+        score, str = playRound(players[p1](), players[p2](), score, i)    
+        console.log(str)        
     }
     if(score[p1] == score[p2]){
         console.log("It was a draw at ${} points each!", score[p1])
@@ -53,3 +56,15 @@ function playerGame(){
     players = {[p1]: playerMove, [p2]: computerMove}
     game(players, rounds)
 }
+
+function genDummyGame() {
+
+}
+
+(function loop() {
+    const rand = Math.round(Math.random() * (3000 - 500)) + 500;
+    setTimeout(() => {
+        genDummyGame();
+        loop();
+    }, rand);
+})
